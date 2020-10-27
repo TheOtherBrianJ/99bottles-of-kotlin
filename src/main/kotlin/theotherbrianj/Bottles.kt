ff60194bd829e296e9147ee56d87dcb7ef845cbf
Brian Johnson <32816710+TheOtherBrianJ@users.noreply.github.com>
2020-10-26 18:48:06 -0700

    first verse

--- src/main/kotlin/theotherbrianj/Bottles.kt
+++ src/main/kotlin/theotherbrianj/Bottles.kt
@@ -1,3 +1,9 @@
 package theotherbrianj
 
-class Bottles
+class Bottles {
+  fun verse(num: Int) = """
+    99 bottles of beer on the wall, 99 bottles of beer.
+    Take one down and pass it around, 98 bottles of beer on the wall
+  """.trimIndent()
+
+}
--- src/test/kotlin/theotherbrianj/BottlesTest.kt
+++ src/test/kotlin/theotherbrianj/BottlesTest.kt
@@ -1,11 +1,22 @@
 package theotherbrianj
 
-import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class BottlesTest {
+  private val unit = Bottles()
+
   @Test
   fun it_is_initializable() {
-    assertThat(Bottles()).isInstanceOf(Bottles::class.java)
+    assertThat(unit).isInstanceOf(Bottles::class.java)
+  }
+
+  @Test
+  fun first_verse() {
+    val expected = """
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall
+    """.trimIndent()
+    assertThat(unit.verse(99)).isEqualTo(expected)
   }
 }
