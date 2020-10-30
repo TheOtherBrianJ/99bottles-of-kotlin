23efcd5279a406dcea15ca2137d201b7547d8445
Brian Johnson <32816710+TheOtherBrianJ@users.noreply.github.com>
2020-10-29 22:01:50 -0700

    verse 0

--- src/main/kotlin/theotherbrianj/Bottles.kt
+++ src/main/kotlin/theotherbrianj/Bottles.kt
@@ -2,20 +2,25 @@ package theotherbrianj
 
 class Bottles {
   fun verse(num: Int) = when (num) {
+    0 -> """
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall
+
+    """.trimIndent()
     1 -> """
-    1 bottle of beer on the wall, 1 bottle of beer.
-    Take it down and pass it around, no more bottles of beer on the wall
+      1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall
 
-  """.trimIndent()
+    """.trimIndent()
     2 -> """
-    $num bottles of beer on the wall, $num bottles of beer.
-    Take one down and pass it around, ${num - 1} bottle of beer on the wall
+      $num bottles of beer on the wall, $num bottles of beer.
+      Take one down and pass it around, ${num - 1} bottle of beer on the wall
 
-  """.trimIndent()
+    """.trimIndent()
     else -> """
-    $num bottles of beer on the wall, $num bottles of beer.
-    Take one down and pass it around, ${num - 1} bottles of beer on the wall
+      $num bottles of beer on the wall, $num bottles of beer.
+      Take one down and pass it around, ${num - 1} bottles of beer on the wall
 
-  """.trimIndent()
+    """.trimIndent()
   }
 }
--- src/test/kotlin/theotherbrianj/BottlesTest.kt
+++ src/test/kotlin/theotherbrianj/BottlesTest.kt
@@ -50,4 +50,14 @@ class BottlesTest {
     """.trimIndent()
     assertThat(unit.verse(1)).isEqualTo(expected)
   }
+
+  @Test
+  fun verse_0() {
+    val expected = """
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall
+
+    """.trimIndent()
+    assertThat(unit.verse(0)).isEqualTo(expected)
+  }
 }
