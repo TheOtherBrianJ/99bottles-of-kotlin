bd71fe0d043fa72131d2edd619a33824b0ad8eea
Brian Johnson <32816710+TheOtherBrianJ@users.noreply.github.com>
2020-11-13 01:00:29 -0700

    refactor verse()

--- src/main/kotlin/theotherbrianj/Bottles.kt
+++ src/main/kotlin/theotherbrianj/Bottles.kt
@@ -1,28 +1,22 @@
 package theotherbrianj
 
 class Bottles {
-  fun verse(num: Int) = when (num) {
-    0 -> """
-      No more bottles of beer on the wall, no more bottles of beer.
-      Go to the store and buy some more, 99 bottles of beer on the wall
+  fun verse(num: Int) = """
+      ${quantity(num).capitalize()} ${container(num)} of beer on the wall, ${quantity(num)} ${container(num)} of beer.
+      ${action(num)}, ${quantity(successor(num))} ${container(successor(num))} of beer on the wall
 
     """.trimIndent()
-    1 -> """
-      1 bottle of beer on the wall, 1 bottle of beer.
-      Take it down and pass it around, no more bottles of beer on the wall
 
-    """.trimIndent()
-    2 -> """
-      $num bottles of beer on the wall, $num bottles of beer.
-      Take one down and pass it around, ${num - 1} bottle of beer on the wall
+  fun successor(num: Int) = if (num == 0) 99 else num - 1
 
-    """.trimIndent()
-    else -> """
-      $num bottles of beer on the wall, $num bottles of beer.
-      Take one down and pass it around, ${num - 1} bottles of beer on the wall
+  fun action(num: Int) =
+    if (num == 0) "Go to the store and buy some more" else "Take ${pronoun(num)} down and pass it around"
 
-    """.trimIndent()
-  }
+  fun quantity(num: Int) = if (num == 0) "no more" else "$num"
+
+  fun pronoun(num: Int) = if (num == 1) "it" else "one"
+
+  fun container(num: Int) = if (num == 1) "bottle" else "bottles"
 
   fun verses(starting: Int, ending: Int) = starting.downTo(ending).joinToString("\n") { verse(it) }
 
